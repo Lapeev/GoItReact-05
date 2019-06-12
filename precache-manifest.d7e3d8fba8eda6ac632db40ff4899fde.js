4345998cb088935570dc07a786cc6eb36b18dc91
lapeev16@gmail.com <lapeev16@gmail.com>
2019-06-12 21:25:54 +0300

    Updates

---
+++ precache-manifest.d7e3d8fba8eda6ac632db40ff4899fde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3accd827e71f52b54ade",
+    "url": "/GoItReact-05/static/js/runtime~main.e1772cfa.js"
+  },
+  {
+    "revision": "a976f47aa98af48e54ee",
+    "url": "/GoItReact-05/static/js/main.5ca55388.chunk.js"
+  },
+  {
+    "revision": "8d2b048c93034fa3856c",
+    "url": "/GoItReact-05/static/js/2.7280a9cf.chunk.js"
+  },
+  {
+    "revision": "a976f47aa98af48e54ee",
+    "url": "/GoItReact-05/static/css/main.aaf3b738.chunk.css"
+  },
+  {
+    "revision": "c90a3b7b32c600a73a8d38e9b5a5f4c0",
+    "url": "/GoItReact-05/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/GoItReact-05/precache-manifest.d7e3d8fba8eda6ac632db40ff4899fde.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/GoItReact-05/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
